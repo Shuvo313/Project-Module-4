5622e4ffd545a87fbc22a1625a6ecdc2c0285009
Shuvo313 <shuvonaim121@gmail.com>
2025-07-26 09:26:42 +0600

    Task not done

---
+++ project/app/classes/VehicleManager.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'VehicleBase.php';
+require_once 'VehicleActions.php';
+require_once 'FileHandler.php';
+
+class VehicleManager extends VehicleBase implements VehicleActions {
+    use FileHandler;
+
+    public function addVehicle($data) {
+       $vehicles = $this->readFile();
+       $vehicles[] = $data;
+       $this->writeFile($vehicles);
+    }
+
+    public function editVehicle($id, $data) {
+        
+    }
+
+    public function deleteVehicle($id) {
+        
+    }
+
+    public function getVehicles() {
+       
+    }
+
+    // Implement abstract method
+    public function getDetails() {
+        
+    }
+}
